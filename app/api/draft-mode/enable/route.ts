a321faccd8fa6a787ab4283a886cb32a0d3d05b6
feledori <felix@felixmayr.it>
2025-12-05 16:08:48 +0100

    cleanup

--- app/api/draft-mode/enable/route.ts
+++
@@ -1,7 +0,0 @@
-import { defineEnableDraftMode } from 'next-sanity/draft-mode'
-import { client } from '~/integrations/sanity/client'
-import { privateToken } from '~/integrations/sanity/env'
-
-export const { GET } = defineEnableDraftMode({
-  client: client.withConfig({ token: privateToken }),
-})
