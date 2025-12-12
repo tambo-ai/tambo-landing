2454a6ed2a4a24e910c6dc69ef011e3725e700c9
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2025-12-12 10:19:08 -0300

    pre mobile section-12

--- app/(pages)/home/_sections/section-12/index.tsx
+++ app/(pages)/home/_sections/section-12/index.tsx
@@ -7,8 +7,8 @@ import s from './section-12.module.css'
 
 export function Section12() {
   return (
-    <section className="dr-layout-grid-inner  dt:dr-pt-122 dt:dr-pb-137">
-      <TitleBlock className="col-start-4 col-end-10 dt:dr-mb-56">
+    <section className="dt:dr-layout-grid-inner  dt:dr-pt-122 dt:dr-pb-137">
+      <TitleBlock className="dt:col-start-4 dt:col-end-10 dt:dr-mb-56">
         <TitleBlock.LeadIn>
           {'<'} PRICING {'>'}
         </TitleBlock.LeadIn>
@@ -18,15 +18,15 @@ export function Section12() {
           from hobbyists to enterprise
         </TitleBlock.Title>
       </TitleBlock>
-      <div className="col-start-2 col-end-12">
-        <div className="grid grid-cols-3 dr-gap-24 dt:dr-mb-32">
+      <div className="dt:col-start-2 dt:col-end-12">
+        <div className="dt:grid dt:grid-cols-3 dt:dr-gap-24 dt:dr-mb-32">
           {pricingCards.map((card, i) => (
             <div
               key={`${card?.plan}-${i}`}
-              className="dt:dr-p-8 border border-dark-grey dr-rounded-20 bg-white dt:dr-h-497 flex flex-col justify-between"
+              className="dt:dr-p-8 border border-dark-grey dt:dr-rounded-20 bg-white dt:dr-h-497 dt:flex dt:flex-col dt:justify-between"
             >
               <div>
-                <div className="dr-p-16 dr-rounded-12 bg-off-white border border-dark-grey dt:dr-mb-32">
+                <div className="dt:dr-p-16 dt:dr-rounded-12 bg-off-white border border-dark-grey dt:dr-mb-32">
                   <p className="typo-label-m dt:dr-mb-16">
                     {'< '}
                     {card?.plan}
@@ -35,17 +35,22 @@ export function Section12() {
                   <h2 className="typo-h3 dt:dr-mb-8">{card?.title}</h2>
                   <p className="typo-p">{card?.description}</p>
                 </div>
-                <ul className="flex flex-col dr-gap-12">
+                <ul className="dt:flex dt:flex-col dt:dr-gap-12">
                   {card?.features.map((feature) => (
-                    <li key={feature} className="flex items-center dr-gap-4">
+                    <li
+                      key={feature}
+                      className="dt:flex dt:items-center dt:dr-gap-4"
+                    >
                       <CheckSVG className="dr-size-16 text-teal" />
                       <p className="typo-label-s">{feature}</p>
                     </li>
                   ))}
                 </ul>
               </div>
-
-              <CTA className="w-full justify-between" href={card?.button?.href}>
+              <CTA
+                className="w-full dt:justify-between"
+                href={card?.button?.href}
+              >
                 {card?.button?.text}
               </CTA>
             </div>
@@ -54,19 +59,19 @@ export function Section12() {
         {/* BANNER */}
         <div
           className={cn(
-            'w-full dt:dr-p-8 border border-dark-grey  dr-rounded-20',
+            'w-full dt:dr-p-8 border border-dark-grey  dt:dr-rounded-20',
             s.banner
           )}
         >
-          <div className="bg-black w-full dr-rounded-12 dr-p-24 relative overflow-hidden flex  justify-between">
+          <div className="bg-black w-full dt:dr-rounded-12 dt:dr-p-24 relative overflow-hidden dt:flex dt:justify-between">
             <div className="absolute inset-0 dark-teal-pattern z-0" />
             <div className="relative text-teal">
               <h3 className="typo-h3 dt:dr-mb-8">{banner?.title}</h3>
               <p className="typo-p">{banner?.description}</p>
             </div>
-            <ul className="flex dr-gap-40 relative">
+            <ul className="dt:flex dt:dr-gap-40 relative">
               {banner?.features.map((feature) => (
-                <li key={feature} className="flex items-center dr-gap-4">
+                <li key={feature} className="dt:flex items-center dr-gap-4">
                   <CheckSVG className="dr-size-16 text-teal" />
                   <p className="typo-label-s text-teal">{feature}</p>
                 </li>
