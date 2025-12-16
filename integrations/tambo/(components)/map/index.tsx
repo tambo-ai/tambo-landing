556ba269de4bb5496330c1308ce8b2a4453937e4
lsoengas <leandro@darkroom.engineering>
2025-12-16 11:31:37 -0300

    remove map page

--- integrations/tambo/(components)/map/index.tsx
+++ integrations/tambo/(components)/map/index.tsx
@@ -1,6 +1,6 @@
 import { withInteractable } from "@tambo-ai/react";
 import { z } from "zod";
-import { AreaSelectMap } from "~/app/(pages)/map/(components)/map";
+import { AreaSelectMap } from "~/integrations/tambo/(components)/area-select-map";
 
 function MapComponent({ height }: { height: number }) {
   return (
@@ -13,7 +13,7 @@ function MapComponent({ height }: { height: number }) {
 export const InterctableMap = withInteractable(MapComponent, {
   componentName: 'map',
   description: 'A map component for selecting an area on a map and analyzing the area for things to do and add pins to the map',
-  propsSchema: z.object({
-    height: z.number().optional().default(356),
-  }),
+  // propsSchema: z.object({
+  //   height: z.number().optional().default(356),
+  // }),
 })
