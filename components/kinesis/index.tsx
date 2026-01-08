0f4e6fc193d722f84ef6c57b49973df4dfba074d
Clément Roche <rchclement@gmail.com>
2026-01-08 19:26:58 +0100

    disable kinesis mobile

--- components/kinesis/index.tsx
+++ components/kinesis/index.tsx
@@ -1,6 +1,7 @@
 import gsap from 'gsap'
 import { useRef } from 'react'
 import { useTempus } from 'tempus/react'
+import { useDeviceDetection } from '~/hooks/use-device-detection'
 import { useMouseMove } from '~/hooks/use-mouse-move'
 
 export function Kinesis({
@@ -16,8 +17,11 @@ export function Kinesis({
 }) {
   const elementRef = useRef<HTMLDivElement>(null)
   const mouseRef = useRef({ x: 0, y: 0 })
+  const { isDesktop } = useDeviceDetection()
 
   useMouseMove((x, y) => {
+    if (!isDesktop) return
+
     gsap.to(mouseRef.current, {
       x: x,
       y: y,
