9476b17e6d4f2f206c055d5d691713498015ab62
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2026-01-20 17:58:29 -0300

    disable kinesis

--- components/kinesis/index.tsx
+++ components/kinesis/index.tsx
@@ -1,7 +1,7 @@
 import cn from 'clsx'
 import gsap from 'gsap'
 import { useRef } from 'react'
-import { useTempus } from 'tempus/react'
+// import { useTempus } from 'tempus/react'
 import { useDeviceDetection } from '~/hooks/use-device-detection'
 import { useMouseMove } from '~/hooks/use-mouse-move'
 
@@ -31,13 +31,13 @@ export function Kinesis({
     })
   })
 
-  useTempus(() => {
-    const _index = typeof getIndex === 'function' ? getIndex() : index
+  // useTempus(() => {
+  //   const _index = typeof getIndex === 'function' ? getIndex() : index
 
-    if (elementRef.current && _index) {
-      elementRef.current.style.transform = `translate(${mouseRef.current.x * _index}px, ${mouseRef.current.y * _index}px)`
-    }
-  })
+  //   if (elementRef.current && _index) {
+  //     elementRef.current.style.transform = `translate(${mouseRef.current.x * _index}px, ${mouseRef.current.y * _index}px)`
+  //   }
+  // })
 
   return (
     <div
