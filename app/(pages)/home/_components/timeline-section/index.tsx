90e23e4ff5afca278481384cdbe6208be12fa773
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2026-01-26 17:02:55 -0300

    max-h

--- app/(pages)/home/_components/timeline-section/index.tsx
+++ app/(pages)/home/_components/timeline-section/index.tsx
@@ -131,7 +131,7 @@ export function TimelineSection({
           ref?.(node)
         }}
       >
-        <div className="dr-layout-grid-inner dt:h-screen relative">
+        <div className="dr-layout-grid-inner dt:h-screen dt:dr-max-h-900 relative border">
           <div className="col-span-4 flex flex-col dr-pt-80 dt:dr-pt-112 max-dt:dr-pb-16 max-dt:h-screen z-1">
             <div className="relative">
               <h3 className="relative typo-h1 dt:typo-h2 text-center dt:text-left z-10">
