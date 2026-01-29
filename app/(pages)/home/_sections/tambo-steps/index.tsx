700cdae049f14341101c70f62ec5265723c11bad
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2026-01-29 16:12:08 -0300

    moment-1 snap

--- app/(pages)/home/_sections/tambo-steps/index.tsx
+++ app/(pages)/home/_sections/tambo-steps/index.tsx
@@ -1,12 +1,15 @@
 'use client'
+import { useLenisSnap } from '~/app/(pages)/_components/lenis/snap'
 import { TimelineSection } from '~/app/(pages)/home/_components/timeline-section'
 import { RiveWrapper } from '~/components/rive'
 import { messages } from './data'
 
 export function TamboSteps() {
+  const setSnapRef = useLenisSnap('center')
+
   return (
     <TimelineSection
-      // ref={setTimelineRectRef}
+      ref={setSnapRef}
       id="moment-1"
       messages={messages}
       href="https://docs.tambo.co/concepts/components"
