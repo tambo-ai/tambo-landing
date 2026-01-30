ea5b03637c08eef2ad391a51a20cd70e3235f515
lsoengas <leandro@darkroom.engineering>
2026-01-30 15:16:08 -0300

    minor bug fixing footer mobile

--- app/(pages)/home/_sections/comunity/index.tsx
+++ app/(pages)/home/_sections/comunity/index.tsx
@@ -15,7 +15,7 @@ type ActiveCard = number | null
 
 export function Community() {
   return (
-    <section className="dt:dr-pt-132 dt:dr-pb-200 dr-pb-120 section-rounded-bottom section-shadow-bottom z-1 bg-light-gray">
+    <section className="dt:dr-pt-132 dt:dr-pb-200 dr-pb-120 section-rounded-bottom section-shadow-bottom z-1 bg-light-gray dt:mb-0 -dr-mb-30">
       <div className="flex flex-col items-center dr-mb-56 dt:dr-px-0 mx-auto px-safe">
         <TitleBlock className="dr-mb-32 dt:mb-0">
           <TitleBlock.Title
