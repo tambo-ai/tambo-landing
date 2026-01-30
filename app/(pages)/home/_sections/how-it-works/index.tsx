f44a4bb1c9a9144728b302b52a94ba3a4bcae319
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2026-01-30 10:03:08 -0300

    how it works tweaks

--- app/(pages)/home/_sections/how-it-works/index.tsx
+++ app/(pages)/home/_sections/how-it-works/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import cn from 'clsx'
-import { useLenisSnap } from '~/app/(pages)/_components/lenis/snap'
+// import { useLenisSnap } from '~/app/(pages)/_components/lenis/snap'
 import { TitleBlock } from '~/app/(pages)/home/_components/title-block'
 import HowItWorksSVG from '~/assets/svgs/how-it-works/how-it-works-diagram-desktop.svg'
 import HowItWorksMobileSVG from '~/assets/svgs/how-it-works/how-it-works-diagram-mobile.svg'
@@ -9,33 +9,36 @@ import { CTA } from '~/components/button'
 import { Image } from '~/components/image'
 
 export function HowItWorks() {
-  const setSnapRef = useLenisSnap('center')
+  // const setSnapRef = useLenisSnap('center')
 
   return (
-    <section
-      ref={setSnapRef}
-      className={cn('dt:dr-pb-200 dt:dr-px-40 dr-px-8 z-1 bg-white')}
-    >
-      <TitleBlock className="dr-mb-56">
-        <TitleBlock.LeadIn>how It Works</TitleBlock.LeadIn>
-        <TitleBlock.Title level="h2" className="dt:mb-0! dr-mb-8">
-          The missing layer <br /> between React and LLMs
-        </TitleBlock.Title>
-        <CTA
-          href="https://docs.tambo.co"
-          className="w-fit mx-auto dt:dr-mt-40 dr-mt-32"
-        >
-          learn more
-        </CTA>
-      </TitleBlock>
-      <div className="content-max-width dr-px-16 dr-py-56 dt:dr-py-0 dr-rounded-20 flex overflow-hidden items-center justify-center relative w-full h-full">
+    <section className={cn('dt:dr-p-40 dr-px-8 dt:dr-px-40 bg-white')}>
+      <div className="z-1 overflow-hidden dr-rounded-20 dt:dr-py-80 dr-py-56 dr-px-16 dt:dr-px-0 flex flex-col relative content-max-width">
+        <TitleBlock className="dr-mb-56">
+          <TitleBlock.LeadIn>how It Works</TitleBlock.LeadIn>
+          <TitleBlock.Title level="h2" className="dt:dr-mb-40! dr-mb-8">
+            The missing layer <br /> between React and LLMs
+          </TitleBlock.Title>
+          <CTA href="https://docs.tambo.co" className="w-fit mx-auto">
+            learn more
+          </CTA>
+        </TitleBlock>
+        <div className="content-max-width">
+          <HowItWorksSVG className="desktop-only dr-w-946 dr-h-430 relative" />
+          <HowItWorksMobileSVG className="mobile-only relative" />
+        </div>
         <Image
           src={'/assets/how-it-works/how-it-works-bg-desktop.png'}
           alt="how it works background"
           fill
+          className="absolute inset-0 -z-1 desktop-only"
         />
-        <HowItWorksSVG className="desktop-only dr-w-1180 dr-h-804 relative" />
-        <HowItWorksMobileSVG className="mobile-only relative" />
+        <Image
+          src={'/assets/how-it-works/how-it-works-bg-mobile.png'}
+          alt="how it works background"
+          fill
+          className="absolute inset-0 -z-1 mobile-only"
+        />
       </div>
     </section>
   )
