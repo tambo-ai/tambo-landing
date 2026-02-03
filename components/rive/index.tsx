2305ea24464eb7db00e686e932f1938e44586560
lsoengas <leandro@darkroom.engineering>
2026-02-03 09:51:36 -0300

    rollback

--- components/rive/index.tsx
+++ components/rive/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Alignment, Fit, Layout, useRive } from '@rive-app/react-canvas'
+import { Alignment, Fit, Layout, useRive } from '@rive-app/react-webgl2'
 import cn from 'clsx'
 import { useIntersectionObserver } from 'hamo'
 import { useEffect } from 'react'
