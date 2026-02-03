bcaa58f482a22f8462d105301e243ae94308ec11
lsoengas <leandro@darkroom.engineering>
2026-02-02 21:38:56 -0300

    add mapbox enable flag fromenvs

--- integrations/tambo/constants.ts
+++ integrations/tambo/constants.ts
@@ -1,5 +1,9 @@
 import type { Destination } from '.'
 
+// Feature flag - set NEXT_PUBLIC_MAPBOX_ENABLED=true to enable Mapbox integration
+export const MAPBOX_ENABLED =
+  process.env.NEXT_PUBLIC_MAPBOX_ENABLED === 'true'
+
 export const DEMOS = {
   INTRO: 'intro',
   SEAT: 'seat',
