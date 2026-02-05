f0d7431cd230ae4afc590471c061ff7548db2564
akhileshrangani4 <akhileshrangani4@gmail.com>
2026-02-04 17:42:44 -0800

    feat(legal): add local MDX pages for License, Privacy, and Terms of Use; implement legal layout and styles

---
+++ app/(pages)/(legal)/layout.tsx
@@ -0,0 +1,36 @@
+import type { ReactNode } from 'react'
+import { Theme } from '~/app/(pages)/_components/theme'
+import { Wrapper } from '~/app/(pages)/_components/wrapper'
+import { FooterContent } from '~/components/footer-content'
+import { getDiscordMembers } from '~/libs/discord'
+import { getGitHubStars } from '~/libs/github'
+import { ScrollToTop } from '~/libs/scroll-to-top'
+import s from './legal.module.css'
+
+interface LegalLayoutProps {
+  children: ReactNode
+}
+
+export default async function LegalLayout({ children }: LegalLayoutProps) {
+  const [githubStars, discordMembers] = await Promise.all([
+    getGitHubStars(),
+    getDiscordMembers(),
+  ])
+
+  return (
+    <Theme theme="light" global>
+      <ScrollToTop />
+      <div className="min-h-dvh flex flex-col bg-white">
+        <Wrapper githubStars={githubStars} discordMembers={discordMembers} />
+        <main className="flex-1 dr-pt-80">
+          <div className="dr-layout-grid-inner dr-py-64">
+            <article className={`col-span-full dt:col-start-4 dt:col-end-10 ${s.prose}`}>
+              {children}
+            </article>
+          </div>
+        </main>
+        <FooterContent />
+      </div>
+    </Theme>
+  )
+}
