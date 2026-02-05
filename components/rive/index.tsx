3eb9f60f19bddc5a5fbdb839fb502a989a8f5c69
lsoengas <leandro@darkroom.engineering>
2026-02-05 14:56:39 -0300

    Referenced assets optimization for moment 1 rive

--- components/rive/index.tsx
+++ components/rive/index.tsx
@@ -1,9 +1,15 @@
 'use client'
 
-import { Alignment, Fit, Layout, useRive } from '@rive-app/react-webgl2'
+import {
+  Alignment,
+  type FileAsset,
+  Fit,
+  Layout,
+  useRive,
+} from '@rive-app/react-webgl2'
 import cn from 'clsx'
 import { useIntersectionObserver } from 'hamo'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 
 interface RiveWrapperProps {
   className?: string
@@ -22,11 +28,35 @@ export function RiveWrapper({
     threshold: 0.3,
   })
 
+  const assetLoader = useCallback((asset: FileAsset, bytes: Uint8Array) => {
+    // If bytes has data, the asset is embedded - let Rive handle it
+    if (bytes.length > 0) {
+      return false
+    }
+
+    // Load referenced fonts
+    if (asset.isFont) {
+      fetch('/fonts/Sentient/Sentient-Light.woff2')
+        .then((res) => res.arrayBuffer())
+        .then((buffer) => asset.decode(new Uint8Array(buffer)))
+      return true
+    }
+    // Load referenced images (Rive doesn't include extension, so append .webp)
+    if (asset.isImage) {
+      fetch(`/assets/rives/images/${asset.name}.webp`)
+        .then((res) => res.arrayBuffer())
+        .then((buffer) => asset.decode(new Uint8Array(buffer)))
+      return true
+    }
+    return false
+  }, [])
+
   const { RiveComponent, rive } = useRive({
     src,
     autoplay: false,
     stateMachines: 'MainStateMachine',
     autoBind: true,
+    assetLoader,
     layout: new Layout({
       fit: Fit[fit],
       alignment: Alignment[alignment],
