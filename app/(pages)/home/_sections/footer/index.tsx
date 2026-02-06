dd78fe2c3050ed31465c896408f3ac2cf4f51ae9
michael magan <magan@tambo.co>
2026-02-05 16:51:23 -0800

    Update CTAs, links, and feature buttons across landing page (#28)

--- app/(pages)/home/_sections/footer/index.tsx
+++ app/(pages)/home/_sections/footer/index.tsx
@@ -10,11 +10,11 @@ import { FooterContent } from '~/components/footer-content'
 import { Image } from '~/components/image'
 import { useDesktopVW } from '~/hooks/use-device-values'
 import { useScrollTrigger } from '~/hooks/use-scroll-trigger'
+import { siteConfig } from '~/libs/config'
 import { fromTo, mapRange } from '~/libs/utils'
 import Background, {
   type BackgroundRefType,
 } from '../../_components/background'
-import { siteConfig } from '~/libs/config'
 
 export function Footer() {
   const [setRectRef, rect] = useRect({ ignoreTransform: true })
@@ -173,7 +173,7 @@ export function Footer() {
                     className="bg-black! text-teal border-teal"
                     onMouseEnter={onMouseEnter}
                     onMouseLeave={onMouseLeave}
-                    href={siteConfig.links.docs}
+                    href={siteConfig.links.dashboard}
                   >
                     START BUILDING
                   </CTA>
