fcebdc2e59e48477f7ddfd2dc5943700ee4465e2
Michael <mike.michaelmilstead@gmail.com>
2026-02-09 12:53:42 -0800

    update colors of eyebrow banner

--- app/(pages)/home/_sections/hero/eyebrow-banner.tsx
+++ app/(pages)/home/_sections/hero/eyebrow-banner.tsx
@@ -11,10 +11,10 @@ export function EyebrowBanner({
   return (
     <Link
       href={href}
-      className="group/pill relative inline-flex items-center rounded-full dr-px-16 dr-py-8 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f]"
+      className="group/pill relative inline-flex items-center rounded-full dr-px-16 dr-py-8 shadow-[inset_0_-8px_10px_#80fcc21f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#80fcc23f]"
     >
       <span
-        className="animate-gradient absolute inset-0 block size-full rounded-[inherit] bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 bg-[length:300%_100%] p-px"
+        className="animate-gradient absolute inset-0 block size-full rounded-[inherit] bg-gradient-to-r from-[#80fcc2]/50 via-[#40a0ff]/50 to-[#80fcc2]/50 bg-[length:300%_100%] p-px"
         style={{
           WebkitMask:
             'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
@@ -26,7 +26,7 @@ export function EyebrowBanner({
       />
       🎉
       <hr className="mx-2 h-4 w-px shrink-0 bg-neutral-500" />
-      <AnimatedGradientText className="typo-p font-medium">
+      <AnimatedGradientText className="typo-p font-medium" colorFrom="#80fcc2" colorTo="#40a0ff">
         {children}
       </AnimatedGradientText>
     </Link>
